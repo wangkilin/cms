2634a4971768ccd9e8b7968cccf8995641d90f69
Zaixin WANG <wangzx@oradt.com>
2015-07-01 17:55:07 +0800

    [code] 添加node.js代码

--- Node.js/httpServer.js
+++ Node.js/httpServer.js
@@ -1,14 +1,21 @@
 var http = require('http');
+var port = 81;
 http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h1>Node.js</h1>');
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    res.write('<h1>欢迎进入Node.js开发</h1>');
 	for(var i in res) {
-	    res.write('<hr>');
-		res.write(i);
-		res.write('<br>');
+	    //res.write('<hr>');
+		//res.write(i);
+		//res.write('<br>');
 		//res.write(res[i].toString());
-		console.info(res[i]);
+		//console.info(res[i]);
+		//sleep(10);
 	}
-    res.end('<p>Hello World</p>');
-}).listen(3000);
-console.log("HTTP server is listening at port 3000.");
+	setTimeout(function () {
+	    console.info(Math.random());
+	    //res.end('<p>Hello World!</p>');
+	}, 10000);
+	res.end('<p>Hello World!</p>');
+    
+}).listen(port);
+console.log("HTTP server is listening at port " + port);
---
+++ Node.js/requirePackage.js
@@ -0,0 +1,6 @@
+
+var Classes = require('./MyPackage'); // 目录下， 只有index.js会被引用进来
+
+
+//Classes.echoName(); // index.js方式
+Classes.echoName2(); // package.json 方式

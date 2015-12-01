f421e5ac32fe0ff2c0e298d60c65143d4e3abfab
Zaixin WANG <wangzx@oradt.com>
2015-12-01 10:30:05 +0800

    [code] 添加H5滚动风格页面样式

--- CodeIgniter-develop/englishday/config/database.php
+++ CodeIgniter-develop/englishday/config/database.php
@@ -76,10 +76,10 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
+	'username' => 'root',
+	'password' => '4728999',
 	'database' => '',
-	'dbdriver' => 'mysqli',
+	'dbdriver' => 'pdo',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),

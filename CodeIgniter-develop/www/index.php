6c1be8a42533b50763b06ee4d08b9e1cbfa860b5
Zaixin WANG <wangzx@oradt.com>
2015-12-01 10:30:05 +0800

    [code] 添加H5滚动风格页面样式

--- CodeIgniter-develop/www/index.php
+++ CodeIgniter-develop/www/index.php
@@ -84,6 +84,7 @@ switch ($workingMode)
 	break;
 
 	default:
+	    break;
 		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
 		echo 'The application environment is not set correctly.';
 		exit(1); // EXIT_ERROR
